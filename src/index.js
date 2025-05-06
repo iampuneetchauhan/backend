1d175e65fb6674d654293e2e3e0fa1a441494bb7
puneet <puneet.carvia@gmail.com>
2025-05-06 12:46:32 +0530

    cors config upgraded

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use("/", router);
 
 const startServer = async () => {
